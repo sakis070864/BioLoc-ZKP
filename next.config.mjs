c4d67bb6a01465275c75fa05cfe079432a2b05e4
sakis070864 <mastorematas@gmail.com>
2026-02-18 21:59:46 +0100

    Update CSP for images and fix ZKP/Sync types

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self' https://api.ipify.org https://*.firebaseio.com https://*.googleapis.com; frame-ancestors 'none';",
+                        value: "default-src 'self' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self' https://api.ipify.org https://*.firebaseio.com https://*.googleapis.com; frame-ancestors 'none';",
                     },
                 ],
             },
